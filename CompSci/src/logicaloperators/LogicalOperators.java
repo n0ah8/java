c7331e29b2f4d4988e0d7e71a2d0afa9c1b75a53
n0ah8 <71708018+n0ah8@users.noreply.github.com>
2024-12-17 11:08:03 -0500

    Add files via upload

---
+++ CompSci/src/logicaloperators/LogicalOperators.java
@@ -0,0 +1,14 @@
+package logicaloperators;
+import java.util.Scanner;
+public class LogicalOperators {
+	public static void main(String[] args) {		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Math marks: ");
+		int math =sc.nextInt();
+		System.out.print("Enter English marks: ");
+		int sicence = sc.nextInt();
+		System.out.print("Enter English marks:");
+		int english = sc.nextInt();
+	}
+
+}
---
+++ CompSci/src/logicaloperators/Quiz.java
@@ -0,0 +1,11 @@
+package logicaloperators;
+import java.util.Scanner;
+public class Quiz {
+
+	public static void main(String[] args) {
+		int score = 10;
+		System.out.println("Welcome to the quiz!");
+		System.out.println("Question 1: Is 2+2 equal to 4?(Yes or no)");
+	}
+
+}
